1f5f0ce69e9629e1b239dfb0fd301d260bcf202e
Ibrahim Yahaya <53970604+OtuwoseIbrahim@users.noreply.github.com>
2022-02-26 00:20:25 +0100

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,11 @@
+from asyncore import file_dispatcher
+
+
+name = input("Pls, enter your name: ")
+
+print("Hello", name+" You are welcome to python world.")
+
+# to output this using the command line,
+# simply use the cd command to navigate
+# to the directory you saved the file
+# use the command "python hello.py"
